22637387960981b0c8ed84b45c8a21bf0d1c9b3e
sigee-min <minshigee@gmail.com>
2024-07-05 00:54:44 +0900

    update: codestyle

--- proxy/src/main/java/com/velocitypowered/proxy/server/ServerGroup.java
+++ proxy/src/main/java/com/velocitypowered/proxy/server/ServerGroup.java
@@ -38,16 +38,14 @@ import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 /**
  * Manages groups of servers and handles updates from an SSE endpoint.
  */
 public class ServerGroup {
-  private static final Logger logger = LoggerFactory.getLogger(ServerGroup.class);
-
+  private static final Logger logger = LogManager.getLogger(ServerGroup.class);
   private final ServerMap serverMap;
   private final Map<String, Set<RegisteredServer>> groupMap;
   private final HttpClient client = HttpClient.newHttpClient();
